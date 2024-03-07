09117c26eb40cc046ad267cf1266ae5ad04d095d
DmitryKnyazkov <kde.2015@yandex.ru>
2024-03-08 01:07:39 +0300

    Прогресс бар работает. при перевороте телефона счетчик не сохраняется.

--- app/src/main/java/com/example/counterdown/MainActivity.kt
+++ app/src/main/java/com/example/counterdown/MainActivity.kt
@@ -2,43 +2,33 @@ package com.example.counterdown
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import com.google.android.material.slider.Slider
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
     var counter = 10
+    var thraedForCounter = ThraedForCounter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         val textCounter: TextView = findViewById(R.id.couter)
-//        val progressBar: ProgressBar = findViewById(R.id.progress_Bar)
+        val progressBar: ProgressBar = findViewById(R.id.progress_Bar)
         val button: Button = findViewById(R.id.button)
         val slider: Slider = findViewById(R.id.slider)
 
-        var thraedForCounter = ThraedForCounter()
 
-//        fun counting() {
-//            var job = scope.launch {
-//
-//                while (counter > 0) {
-//                    delay(1000)
-//                    counter--
-//                    textCounter.setText(counter.toString())
-//                }
-//                slider.isEnabled = true
-//            }
-//        }
+
 
         fun setCounter(secund: Int) {
-//            counter = secund
             thraedForCounter.counter = secund
+            thraedForCounter.counterr = secund
 
             textCounter.setText(thraedForCounter.counter.toString())
         }
@@ -47,19 +37,33 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             if (button.text == "СТАРТ") {
-                thraedForCounter.startCoroutine(textCounter, slider, button, true)
+                thraedForCounter.startCoroutine(textCounter, slider, button, progressBar, true)
 
                 slider.isEnabled = false
                 button.setText("СТОП")
-//                counting()
+
             }
             else {
-                thraedForCounter.startCoroutine(textCounter, slider,  button, false)
+                thraedForCounter.startCoroutine(textCounter, slider,  button, progressBar, false)
                 button.setText("СТАРТ")
                 slider.isEnabled = true
 //                scope.cancel()
             }
         }
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putInt("counter", counter)
+
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+        counter = savedInstanceState?.getInt("counter")?:0
     }
 }
--- app/src/main/java/com/example/counterdown/ThraedForCounter.kt
+++ app/src/main/java/com/example/counterdown/ThraedForCounter.kt
@@ -1,27 +1,25 @@
 package com.example.counterdown
 
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import com.google.android.material.slider.Slider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class ThraedForCounter(counterr: Int = 10) {
+    var counterr = counterr
     var counter = counterr
     val scope = CoroutineScope(Dispatchers.Main)
     var job = scope.launch{}
 
-    //    val counterText by lazy { findViewById<TextView>(R.id.couter) }
     fun startCoroutine(
         textView: TextView,
         slider: Slider,
         button: Button,
+        progressBar: ProgressBar,
         toDo: Boolean
     ) {
         if (toDo) {
@@ -30,6 +28,7 @@ class ThraedForCounter(counterr: Int = 10) {
                     delay(1000)
                     counter--
                     textView.setText(counter.toString())
+                    progressBar.setProgress(counter*100/counterr)
                 }
                 slider.isEnabled = true
                 button.setText("СТАРТ")
